ddb3444df99ec37c6ba3f4853f2625c29397b544
AileneKane <ailene.ettinger@gmail.com>
2022-04-08 16:36:53 -0700

    April temp data

---
+++ simhwbdat.R
@@ -0,0 +1,15 @@
+#helpful website for simulatig: https://aosmith.rbind.io/2018/04/23/simulate-simulate-part-2/#simulate-simulate-dance-to-the-music
+rm(list=ls()) 
+options(stringsAsFactors = FALSE)
+
+# libraries
+library(dplyr)
+library(lme4)
+library(scales)
+#set working directory
+setwd("~/GitHub/grit")
+
+green.b<-.5
+b0<-0
+dist<-seq()
+rnorm(400,mean=)
